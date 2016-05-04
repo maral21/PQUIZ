3cc5a54687847f589d421b3cd2da491a8d7334a0
marandreslopez <marandreslopez@gmail.com>
2016-05-04 16:28:44 +0200

    Marco Aplicación

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var partials = require('express-partials');
 
 
 var app = express();
@@ -21,6 +22,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(partials());
 
 app.use('/', routes);
 
